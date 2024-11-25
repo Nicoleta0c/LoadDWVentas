fe36f2d7d1bf85332080c1bb1ae0b4df8c8907ac
NicoletaOc <niicostars04@gmail.com>
2024-11-24 22:44:29 -0400

    Primer commit

---
+++ LoadDWVentas.Data/Entities/DwVentas/DimShipper.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoadDWVentas.Data.Entities.DwVentas
+{
+
+    [Table("DimShippers")]
+    public class DimShipper
+    {
+
+
+        [Key]
+        public int ShipperKey { get; set; }
+        public int ShipperId { get; set; }
+        public string? ShipperName { get; set; }
+    }
+}
